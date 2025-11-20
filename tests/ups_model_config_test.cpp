36735204be46d0d6357adec988e43396f01047cc
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2025-11-20 17:43:04 +0300

    Bad commit

--- tests/ups_model_config_test.cpp
+++ tests/ups_model_config_test.cpp
@@ -1,5 +1,10 @@
 #include <gtest/gtest.h>
+#include "ups_model_config.h"
 
-TEST(UpsModelConfigTest, SampleTest) {
+TEST(UpsModelConfigTest, LoadTest) {
+    UpsModelConfig cfg;
+
+    bool ok = cfg.load("config/ups_models.ini", "INELT");
+
     EXPECT_TRUE(true);
 }
