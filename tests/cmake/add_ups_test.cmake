6d00acb0da243520267f80db3ff15966d7987414
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2025-11-24 16:06:17 +0300

    cmake функция добавления тестов выделена в отдельный файл

---
+++ tests/cmake/add_ups_test.cmake
@@ -0,0 +1,26 @@
+# @file tests/cmake/add_ups_test.cmake
+# ===== Функция добавления тестов =====
+function(add_ups_test TEST_NAME)
+    add_executable(${TEST_NAME} ${ARGN})
+
+    target_link_libraries(${TEST_NAME} PRIVATE
+        ups_core
+        GTest::gtest_main
+    )
+
+    set_target_properties(${TEST_NAME} PROPERTIES
+        CXX_STANDARD 11
+        CXX_STANDARD_REQUIRED ON
+        CXX_EXTENSIONS OFF
+    )
+
+    target_compile_options(${TEST_NAME} PRIVATE ${WARN_FLAGS})
+
+    add_test(NAME ${TEST_NAME} COMMAND ${TEST_NAME})
+
+    # Включить Coverage для тестов
+    if (BUILD_COVERAGE)
+        target_compile_options(${TEST_NAME} PRIVATE ${COVERAGE_COMPILE_FLAGS})
+        target_link_options(${TEST_NAME} PRIVATE ${COVERAGE_LINK_FLAGS})
+    endif()
+endfunction()
