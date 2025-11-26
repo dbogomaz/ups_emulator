2d39c8dd99f3bd7c71b1b8c99e5e9eae2a574463
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2025-11-26 13:17:29 +0300

    Добавлена утилита для обработки многострочного блока в файле ini

--- src/utils/string_utils.h
+++ src/utils/string_utils.h
@@ -4,12 +4,22 @@
 #include <string>
 
 namespace utils {
-    /**
-     * @brief Удаляет пробельные символы в начале и конце строки
-     * @param s Входная строка
-     * @return Обрезанная строка
-     */
-    std::string trim(const std::string& s);
-} // namespace utils
+/**
+ * @brief Удаляет пробельные символы в начале и конце строки
+ * @param s Входная строка
+ * @return Обрезанная строка
+ */
+std::string trim(const std::string& s);
 
-#endif // STRING_UTILS_H
+/**
+ * @brief Считывает многострочный enum из файла, начиная с первой строки
+ * 
+ * @param file Поток файла для чтения
+ * @param firstLine Первая строка enum
+ * @return std::string Полный многострочный enum в виде строки
+ */
+std::string readMultilineEnum(std::ifstream& file, const std::string& firstLine);
+
+}  // namespace utils
+
+#endif  // STRING_UTILS_H
