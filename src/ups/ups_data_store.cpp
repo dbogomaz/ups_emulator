3ee5bfba683a12f939e1846bbd3970d9f330c1b5
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2025-11-28 10:18:48 +0300

    Добавлен класс UpsDataStore с методами-заглушками

--- src/ups/ups_data_store.cpp
+++ src/ups/ups_data_store.cpp
@@ -1,1 +1,60 @@
 #include "ups_data_store.h"
+
+#include "utils/string_utils.h"
+
+bool UpsDataStore::init(const UpsModelConfig& cfg, ErrorMessage* err) {
+    m_parameters.clear();
+    m_valueSets.clear();
+
+    const UpsOids& oids = cfg.oids();
+
+    // ---- Вспомогательная лямбда для добавления параметра ----
+    auto addParam = [&](const Oid& oid, const UpsParameterName& name,
+                        UpsParameterType type) -> bool {
+        if (oid.empty()) {
+            if (err) *err = "OID for field '" + name + "' is empty";
+            return false;
+        }
+
+        UpsParameter p;
+        p.name = name;
+        p.oid = oid;
+        p.type = type;
+
+        // Значение по умолчанию
+        p.value = (type == UpsParameterType::String ? "" : "0");
+
+        m_parameters[oid] = p;
+        return true;
+    };
+
+    // ---- Добавление всех базовых параметров ----
+    if (!addParam(oids.modelNameOID, "modelName", UpsParameterType::String)) return false;
+    if (!addParam(oids.inputVoltageOID, "inputVoltage", UpsParameterType::Integer)) return false;
+    if (!addParam(oids.inputFreqOID, "inputFreq", UpsParameterType::Integer)) return false;
+    if (!addParam(oids.outputVoltageOID, "outputVoltage", UpsParameterType::Integer)) return false;
+    if (!addParam(oids.batteryStatusOID, "batteryStatus", UpsParameterType::Integer)) return false;
+    if (!addParam(oids.chargeRemainingOID, "chargeRemaining", UpsParameterType::Integer))
+        return false;
+    if (!addParam(oids.batteryTempOID, "batteryTemp", UpsParameterType::Integer)) return false;
+    if (!addParam(oids.outputStatusOID, "outputStatus", UpsParameterType::Integer)) return false;
+
+    // ---- Добавление наборов допустимых значений (ValueSets) ----
+    const FieldValueSets& sets = cfg.definedFields();
+
+    if (!sets.batteryStatusSet.nameToValue.empty())
+        m_valueSets[oids.batteryStatusOID] = &sets.batteryStatusSet;
+
+    if (!sets.outputStatusSet.nameToValue.empty())
+        m_valueSets[oids.outputStatusOID] = &sets.outputStatusSet;
+
+    return true;
+}
+
+bool UpsDataStore::has(const Oid& oid) const { return false; }
+
+const UpsParameter* UpsDataStore::get(const Oid& oid) const { return nullptr; }
+
+bool UpsDataStore::set(const Oid& oid, const UpsParameterValue& value, ErrorMessage* err) {
+    return false;
+}
--- src/ups/ups_data_store.h
+++ src/ups/ups_data_store.h
@@ -1,7 +1,37 @@
 #ifndef UPS_DATA_STORE_H
 #define UPS_DATA_STORE_H
 
+#include <unordered_map>
 
+#include "ups_parameter.h"
+#include "ups_model_config.h"
 
+// Хранилище текущего состояния UPS
+class UpsDataStore {
+public:
+    // Инициализация хранилища по конфигурации
+    bool init(const UpsModelConfig& cfg, ErrorMessage* err = nullptr);
 
-#endif  // UPS_DATA_STORE_H
+    // Проверить, существует ли параметр с данным OID
+    bool has(const Oid& oid) const;
+
+    // Получить параметр по OID (nullptr если нет)
+    const UpsParameter* get(const Oid& oid) const;
+
+    // Установить новое значение параметра
+    bool set(const Oid& oid, const UpsParameterValue& value, ErrorMessage* err = nullptr);
+
+private:
+    // Параметры UPS по ключу OID
+    std::unordered_map<Oid, UpsParameter> m_parameters{};
+
+    // Ограничения для перечислимых параметров (OID → набор значений)
+    std::unordered_map<Oid, const FieldValueSet*> m_valueSets{};
+
+    // Проверка корректности значения
+    bool validateValue(const UpsParameter& param,
+                       const UpsParameterValue& value,
+                       ErrorMessage* err) const;
+};
+
+#endif // UPS_DATA_STORE_H
