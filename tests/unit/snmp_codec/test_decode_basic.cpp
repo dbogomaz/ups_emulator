09f0217f749cdaadfa37dc97938be50138e23905
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2025-12-02 12:28:39 +0300

    Уточнен тест декодера для одного OID

--- tests/unit/snmp_codec/test_decode_basic.cpp
+++ tests/unit/snmp_codec/test_decode_basic.cpp
@@ -3,18 +3,7 @@
 
 using namespace snmp;
 
-// // ASN.1 tags
-// static constexpr uint8_t TAG_SEQUENCE       = 0x30;
-// static constexpr uint8_t TAG_INTEGER        = 0x02;
-// static constexpr uint8_t TAG_STRING         = 0x04;
-// static constexpr uint8_t TAG_NULL           = 0x05;
-// static constexpr uint8_t TAG_OID            = 0x06;
-
-// static constexpr uint8_t TAG_GETREQUEST     = 0xA0;
-// static constexpr uint8_t TAG_GETRESPONSE    = 0xA2;
-
-TEST(SnmpCodecDecodeBasic, DecodeOneOid)
-{
+TEST(SnmpCodecDecodeBasic, DecodeOneOid) {
     SnmpCodec codec;
     SnmpGetRequest req;
     ErrorMessage err;
@@ -25,7 +14,7 @@ TEST(SnmpCodecDecodeBasic, DecodeOneOid)
             0x02, 0x01, 0x00, // version
             0x04, 0x06, 'p','u','b','l','i','c',
             0xA0, 0x18, // GetRequest PDU length 24
-                0x02, 0x04, 0x18, 0x1f, 0x89, 0x6a, // request-id length 4  
+                0x02, 0x04, 0x18, 0x1f, 0x89, 0x6a, // request-id 404719978 length 4  
                 0x02, 0x01, 0x00, // error-status
                 0x02, 0x01, 0x00, // error-index
                 0x30, 0x0a, // VarBindList length 10
@@ -37,9 +26,10 @@ TEST(SnmpCodecDecodeBasic, DecodeOneOid)
     bool ok = codec.decodeGetRequest(data, sizeof(data), req, &err);
     ASSERT_TRUE(ok) << "Decode failed: " << err;
 
-    // EXPECT_EQ(req.requestId, 1) << "Wrong requestId" << req.requestId;
-    // ASSERT_EQ(req.oids.size(), 1) << "Wrong number of OIDs" << req.oids.size();
-    // EXPECT_EQ(req.oids[0], "1.3.6") << "Wrong OID" << req.oids[0];
+    ASSERT_EQ(req.community, "public") << "Wrong community string " << req.community;
+    EXPECT_EQ(req.requestId, 0x181F896A) << "Wrong requestId " << req.requestId;
+    ASSERT_EQ(req.oids.size(), 1) << "Wrong number of OIDs " << req.oids.size();
+    EXPECT_EQ(req.oids[0], "1.3.6.1.2") << "Wrong OID " << req.oids[0];
 }
 
 #if 0
