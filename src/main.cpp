59966ae69798886ccf85fe0f86fba2a69a855e8c
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2025-12-08 17:18:15 +0300

    Успешная проверка через snmpget

--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,44 @@
 #include <iostream>
 
-#include "ups_data_store.h"
-#include "ups_model_config.h"
+#include "snmp/snmp_agent.h"
+#include "ups/ups_data_store.h"
+#include "ups/ups_model_config.h"
 
 int main() {
+    // Загружаем модель (например, из файла ups_models.ini)
     UpsModelConfig cfg;
-
     if (!cfg.load("config/ups_models.ini", "APC")) {
-        std::cout << "Failed to load config\n";
-        std::cout << "Error: " << cfg.lastError() << "\n";
+        std::cerr << "Failed to load UPS model\n";
         return 1;
     }
-#if 0
-    std::cout << "=== UPS Model Config Loaded ===\n\n";
 
-    std::cout << "Model name           : " << cfg.modelName() << "\n";
-    std::cout << "Model name OID       : " << cfg.oids().modelNameOID << "\n\n";
-
-    std::cout << "Input voltage OID    : " << cfg.oids().inputVoltageOID << "\n";
-    std::cout << "Input freq OID       : " << cfg.oids().inputFreqOID << "\n\n";
-
-    std::cout << "Output voltage OID   : " << cfg.oids().outputVoltageOID << "\n";
-    std::cout << "Output status OID    : " << cfg.oids().outputStatusOID << "\n\n";
-
-    std::cout << "Battery status OID   : " << cfg.oids().batteryStatusOID << "\n";
-    std::cout << "Charge remaining OID : " << cfg.oids().chargeRemainingOID << "\n";
-    std::cout << "Battery temp OID     : " << cfg.oids().batteryTempOID << "\n\n";
-
-    // ===== BatteryStatusEnum =====
-    std::cout << "BatteryStatusEnum:\n";
-    const FieldValueSet& bat = cfg.definedFields().batteryStatusSet;
-    for (std::map<std::string, int>::const_iterator it = bat.nameToValue.begin();
-         it != bat.nameToValue.end(); ++it) {
-        std::cout << "  " << it->first << " = " << it->second << "\n";
-    }
-    std::cout << "\n";
-
-    // ===== OutputStatusEnum =====
-    std::cout << "OutputStatusEnum:\n";
-    const FieldValueSet& out = cfg.definedFields().outputStatusSet;
-    for (std::map<std::string, int>::const_iterator it = out.nameToValue.begin();
-         it != out.nameToValue.end(); ++it) {
-        std::cout << "  " << it->first << " = " << it->second << "\n";
-    }
-    std::cout << "\n";
-#endif
-
-    // ========================== Инициализация UpsDataStore ==========================
+    // Инициализируем хранилище параметров
     UpsDataStore store;
     if (!store.init(cfg)) {
-        std::cout << "Failed to init UpsDataStore\n";
+        std::cerr << "Failed to init data store\n";
         return 1;
     }
 
-    auto printParameter = [&store](const Oid& oid) {
-        const UpsParameter* p = store.get(oid);
-        if (p) {
-            std::cout << "Parameter: " << p->name << " OID: " << p->oid << " value: " << p->value
-                      << "\n";
-        } else {
-            std::cout << "Parameter with OID " << oid << " not found\n";
-        }
-    };
+    // устанавливаем значения
+    store.set(cfg.oids().batteryStatusOID, "2");
+    store.set(cfg.oids().batteryTempOID, "23");
+    store.set(cfg.oids().chargeRemainingOID, "78");
+    store.set(cfg.oids().inputFreqOID, "50");
+    store.set(cfg.oids().inputVoltageOID, "230");
+    store.set(cfg.oids().modelNameOID, "Smart-UPS RT 2000 XL");
+    store.set(cfg.oids().outputStatusOID, "2");
+    store.set(cfg.oids().outputVoltageOID, "220");
 
-    Oid testOid;
-    ErrorMessage err;
+    // Создаем SNMP-агент
+    SnmpAgent agent(&store);
 
-    auto testSetParameter = [&store, &printParameter, &err](const Oid& oid,
-                                                            const UpsParameterValue& value) {
-        store.set(oid, value, &err);
-        printf("Setting value: \"%s\"\n", value.c_str());
-        printParameter(oid);
-        if (!err.empty()) {
-            std::cout << "Error setting parameter: " << err << "\n";
-            err.clear();
-        }
-        printf("\n");
-    };
+    if (!agent.start(161)) {
+        std::cerr << "Failed to start SNMP agent\n";
+        return 1;
+    }
 
-    // testSetParameter(cfg.oids().modelNameOID, "New Model 123");
-    // testSetParameter(cfg.oids().inputVoltageOID, "220");
-    // testSetParameter(cfg.oids().inputFreqOID, "50");
-    // testSetParameter(cfg.oids().outputVoltageOID, "bad_value_not_int");  // Ошибка
-    testSetParameter(cfg.oids().batteryStatusOID, "batteryNormal");
-    testSetParameter(cfg.oids().batteryStatusOID, "bad_value");  // Ошибка
-    testSetParameter(cfg.oids().batteryStatusOID, "2");
-    testSetParameter(cfg.oids().batteryStatusOID, "0"); // Ошибка
-
-
-
+    // Переходим в основной цикл
+    agent.run();
 
     return 0;
 }
