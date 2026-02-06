66f97f346140cbf46e03135887e6e386e764a1c7
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2026-02-06 11:15:13 +0300

    cli: добавлен парсер аргументов и ключ --list

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,43 @@
 #include <algorithm>
-#include <iostream>
-#include <csignal>
 #include <atomic>
-#include <thread>
 #include <chrono>
+#include <csignal>
+#include <iostream>
+#include <thread>
 
 #include "ups_emulator.h"
 
-static std::atomic<bool> g_stop{false};
+static std::atomic<bool> g_stop{ false };
+void handleSignal(int);
 
-void handleSignal(int) {
-    g_stop.store(true);
-}
+struct CliOptions {
+    bool showHelp = false;
+    bool listModels = false;
+    std::string modelId;
+    std::string error;
+};
+
+CliOptions parseArgs(int argc, char* argv[]);
+void printHelp(const char* progName);
 
 int main(int argc, char* argv[]) {
     const std::string configPath = "config/ups_models.ini";
 
-    std::signal(SIGINT,  handleSignal);
+    std::signal(SIGINT, handleSignal);
     std::signal(SIGTERM, handleSignal);
+
+    CliOptions opts = parseArgs(argc, argv);
+
+    if (!opts.error.empty()) {
+        std::cerr << "Error: " << opts.error << "\n";
+        std::cerr << "Use --help to see available options.\n";
+        return 1;
+    }
+
+    if (opts.showHelp) {
+        printHelp(argv[0]);
+        return 0;
+    }
 
     UpsEmulator emulator(configPath);
 
@@ -32,21 +52,18 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    std::string cliModel;
-    for (int i = 1; i < argc; ++i) {
-        std::string arg = argv[i];
-        if ((arg == "-m" || arg == "--model") && i + 1 < argc) {
-            cliModel = argv[i + 1];
-            break;
+    if (opts.listModels) {
+        for (const auto& id : models) {
+            std::cout << id << "\n";
         }
+        return 0;
     }
 
-    std::string modelName;
-    if (!cliModel.empty() &&
-        std::find(models.begin(), models.end(), cliModel) != models.end()) {
-        modelName = cliModel;
-    } else {
-        modelName = models.front();
+    std::string modelName = opts.modelId.empty() ? models.front() : opts.modelId;
+
+    if (std::find(models.begin(), models.end(), modelName) == models.end()) {
+        std::cerr << "Unknown UPS model: " << modelName << "\n";
+        return 1;
     }
 
     if (!emulator.selectModel(modelName)) {
@@ -66,4 +83,46 @@ int main(int argc, char* argv[]) {
 
     emulator.stop();
     return 0;
+}
+
+void handleSignal(int) { g_stop.store(true); }
+
+CliOptions parseArgs(int argc, char* argv[]) {
+    CliOptions opts;
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+
+        if (arg == "--help") {
+            opts.showHelp = true;
+            return opts;
+        }
+
+        if (arg == "-l" || arg == "--list") {
+            opts.listModels = true;
+            continue;
+        }
+
+        if (arg == "-m" || arg == "--model") {
+            if (i + 1 >= argc) {
+                opts.error = "Option '" + arg + "' requires an argument";
+                return opts;
+            }
+            opts.modelId = argv[++i];
+            continue;
+        }
+
+        opts.error = "Unknown option: " + arg;
+        return opts;
+    }
+
+    return opts;
+}
+
+void printHelp(const char* progName) {
+    std::cout << "Usage: " << progName << " [OPTIONS]\n\n"
+              << "Options:\n"
+              << "  -l, --list           List available UPS models and exit\n"
+              << "  -m, --model <ID>     Select UPS model by ID\n"
+              << "      --help           Show this help and exit\n";
 }
